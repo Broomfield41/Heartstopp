3a418577bdca03649f9fae484c3b6e03ea2e9fb6
Broomfield41 <broomf661@stcharles.k12.la.us>
2024-01-30 16:42:15 +0000

    saving data shapes

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -74,7 +74,10 @@
        }
 
   // TODO 5-a: add a function that handles the bad display type
-  
+  function handleBad (data, repeat) {
+    setBackgroundWithMixed (data, repeat)
+    animationDetails.displayType = 3
+  }
 
   /////////////////////////////////////////////////
   // BUTTON HANDLERS BELOW HERE (3-b, 4-b, 5-b) ///
@@ -87,12 +90,14 @@
 
   function goodDisplay() {
     // TODO 4-b: call your handleGood function
-    
+    var currentShape = dataShapes.shape
   }
 
   function badDisplay() {
     // TODO 5-b: call your handleBad function
-    
+    var currentShape = dataShapes[i]
+    var repeat = currentShape.shape
+    handleBad(currentShape, repeat)
   }
 
   /////////////////////////////////////////////////
